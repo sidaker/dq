122c5d9eb18c11f6faac862784c29cdc53d9ca34
sidaker <sbommireddy@akersystems.com>
2020-01-11 22:25:20 +0000

    pyspark related code

---
+++ pyspark/wordcount_python.py
@@ -0,0 +1,39 @@
+'''
+This is a Python program that computes word count per line
+This program needs an understanding of below
+    - for loop
+    - string split function
+    - variables (local and global scope)
+    - functions
+    - generators
+    - Reading a file using with
+    - Difference between yield and return
+'''
+
+filepath = "/Users/siddharthabommireddy/Desktop/Python/sidgitrepo/dq/pyspark/welcome.txt"
+
+def wordcount(line):
+    ct=0
+    for word in line.split():
+        ct = ct +1
+    return ct
+
+def readfile(fullfilepath):
+    '''
+    Read each line of the file and return it
+    to calling function using yield.
+    '''
+    with open(fullfilepath) as fp:
+        for line in fp:
+            yield line
+
+def main():
+    for no,line in enumerate(readfile(filepath),1):
+        wc = wordcount(line)
+        # using format() method
+        print('Line {} has a total of "{}!"'.format(no, wc))
+        print(line)
+
+
+if __name__ == '__main__':
+    main()
