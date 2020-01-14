56391c0907af3e6dcffdc170aa9f22081fcc9b43
Siddartha <siddartha.bommireddy@digital.homeoffice.gov.uk>
2020-01-14 20:45:20 +0000

    more changes

--- pyspark/day1.py
+++ pyspark/day1.py
@@ -62,3 +62,65 @@ for num in range(5):
 # providing the start, stop, and step for the range function
 for num in range(2, 10, 2):
  print( "Value: {}".format(num) ) # will print all evens between 2 and 10
+
+# printing all characters in a name using the 'in' keyword
+name = "Lik Bar"
+for letter in name:
+ print( "Value: {}".format(letter) )
+
+# A while loop is generally used when
+# you need to loop based on a condition rather than counting.
+
+# using two or more loops together is called a nested loop
+for i in range(2): # outside loop
+    for j in range(3): # inside loop
+        print( i, j )
+
+# checking the number of items within a list
+nums = [5, 10, 15]
+length = len(nums) # len() returns an integer
+print(length)
+
+# accessing specific items of a list with slices
+print( nums[ 1 : 3 ] ) # will output items in index 1 and 2
+print( nums[ : 2 ] ) # will output items in index 0 and 1
+print( nums[ : : 2 ] ) # will print every other index - 0, 2, 4, etc.
+print( nums[ -2 : ] ) # will output the last two items in list
+
+# using the remove method with a try and except
+sports = [ "baseball", "soccer", "football", "hockey" ]
+try:
+    sports.remove("soccer")
+except:
+    print("That item does not exist in the list")
+print(sports)
+
+
+# using min, max, and sum
+nums = [5, 3, 9]
+print( min(nums) ) # will find the lowest number in the list
+print( max(nums) ) # will find the highest number in the list
+print( sum(nums) ) # will add all numbers in the list and return the sum
+
+
+# using sorted on lists for numerical and alphabetical data
+nums = [5, 8, 0, 2]
+sorted_nums = sorted(nums) # save to a new variable to use later
+print(nums, sorted_nums) # the original list is in tact
+
+
+# sorting a list with .sort() in-place
+'''
+The sort method is used for the same purpose that our previous sorted function is used
+for; however, it will change the original list directly:
+'''
+nums = [5, 0, 8, 3]
+nums.sort( ) # alters the original variable directly
+print(nums)
+
+# using conditional statements on a list
+names = [ "Jack", "Robert", "Mary" ]
+if "Mary" in names:
+    print("found") # will run since Mary is in the list
+if "Jimmy" not in names:
+    print("not found") # will run since Jimmy is not in the list
