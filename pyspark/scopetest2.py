cd76d9e4d96b216360c689b819c4581298080acc
Siddartha <siddartha.bommireddy@digital.homeoffice.gov.uk>
2020-01-18 09:29:54 +0000

    iterators

--- pyspark/scopetest2.py
+++ pyspark/scopetest2.py
@@ -3,7 +3,6 @@ def my_vars():
   print("Global Variable:", global_var) # See how it is different to below
   # global_var = global_var + 1 # UnboundLocalError: local variable 'global_var' referenced before assignment
   # Variables with global scope can be referenced inside a function
-
   local_var = 2 # the scope of the variable is purely local.
   print("Local Variable:",local_var )
   global global_inner_var
