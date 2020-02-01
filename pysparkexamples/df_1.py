ed4079c4de680feb230e15fb06250de341e25c41
Siddhartha Bommireddy <siddharthabommireddy@Siddharthas-iMac.local>
2020-02-01 01:22:24 +0000

    Add

---
+++ pysparkexamples/df_1.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, LongType, StringType, FloatType
+
+# from pyspark import SparkContext
+# spark-submit --master local sampleSpark.py
+# spark-submit --master
+# master node is where the cluster manager is running.
+# local mode vs cluster mode
+# driver runs its own Java process(JVM) so does the Executors which have their own JVM's.
+# spark-submit --executor-memory 3G --total-executor-cores 2 sampleSpark.py
+
+
+spark = SparkSession.builder.appName('mytestSparkapp').getOrCreate()
+
+filename =  "/Users/siddharthabommireddy/Documents/Spark/python-spark-tutorial/in/airports.text"
+
+myManualSchema = StructType([
+StructField("AirportId",LongType(),True),
+StructField("AirportName",StringType(),True),
+StructField("City",StringType(),True),
+StructField("Country",StringType(),True),
+StructField("AirportCode",StringType(),True),
+StructField("ICAOCode",StringType(),True),
+StructField("Latitude",FloatType(),False),
+StructField("Longitude",FloatType(),False),
+StructField("Altitude",FloatType(),False),
+StructField("timezoneDST",StringType(),False),
+StructField("unknown",StringType(),False),
+StructField("timeZoneinOlson",StringType(),False)
+])
+
+airports = spark.read.format("csv").schema(myManualSchema).load(filename)
+# option("inferSchema", "true").option("header","true")
+
+# make the dataframe a table or view.
+airports.createOrReplaceTempView("airports_all")
+
+airportsNamesandCities = spark.sql("""
+SELECT AirportName,City
+FROM airports_all
+WHERE Country = "United States"
+""")
+
+airportsNamesandCities.show()
