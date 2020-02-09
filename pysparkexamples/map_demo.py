40b8a368649ea227595805448d118d15ecf6c6d0
Siddartha <siddartha.bommireddy@digital.homeoffice.gov.uk>
2020-02-09 12:01:44 +0000

    rdds

---
+++ pysparkexamples/map_demo.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('mytestSparkapp').getOrCreate()
+
+list1 = [1,2,3,4,5,6]
+list2 = [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6)]
+
+def sqrt(x):
+    return x*x
+
+rdd1 = spark.sparkContext.parallelize(list1,2)
+
+pairrdd1 = spark.sparkContext.parallelize(list2,2)
+
+maprdd1 = rdd1.map(sqrt).collect()
+
+print("*"*50)
+print("using map on a normal RDD")
+print(maprdd1)
+
+print("*"*50)
+print("using map on a paired RDD")
+mappairrdd1 = pairrdd1.map(lambda x:(x[0],sqrt(x[1]))).collect()
+print(mappairrdd1)
+
+print("*"*50)
+print("using mapValues on a paired RDD")
+mapvaluespairrdd1 = pairrdd1.mapValues(lambda x:sqrt(x)).collect()
+print(mapvaluespairrdd1)
