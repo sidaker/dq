ec8d81575eea22c4fb86bb8bf0e3b4b0f560bda1
Siddartha <siddartha.bommireddy@digital.homeoffice.gov.uk>
2020-02-09 18:54:20 +0000

    rdds

--- pysparkexamples/broad_1.py
+++ pysparkexamples/broad_1.py
@@ -22,4 +22,13 @@ userrdd1 = spark.sparkContext.parallelize(user_data,2)
 userrdd2 = userrdd1.map(lambda x: (x[0],capbroadcast.value.get(x[1],"no idea")))
 
 print(type(userrdd2))
+print("With Broadcast")
+print("*"*50)
 print(userrdd2.collect())
+
+# With out broadcast you would have implemented the code in the below manner.
+print("Without Broadcast")
+print("*"*50)
+userrdd3 = userrdd1.map(lambda x: (x[0],capital_data.get(x[1],"no idea")))
+
+print(userrdd3.collect())
